dfe3ee44e7773a1e1b8d7cac1c888cbb0ffe1463
paronson <paronson@qw-corp.com>
2020-02-25 15:32:16 -0700

    refactor express

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require("path");
+const dotenv = require("dotenv");
+const express = require("express");
+const helmet = require("helmet");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// load env config
+dotenv.config({ path: `${path.resolve(__dirname)}/config/config.env` });
+
+// helmet adds security headers
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+app.listen(PORT, () =>
+    console.log(
+        `Server is listening on port: ${PORT} in ${process.env.NODE_ENV} environment`
+    )
+);
+
